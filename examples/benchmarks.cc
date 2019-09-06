0a2052f3c3977d824afd87db175edae878b86560
Klaas de Vries <klaasjacobdevries@gmail.com>
2019-09-06 08:25:29 +0100

    benchmark ostream

--- examples/benchmarks.cc
+++ examples/benchmarks.cc
@@ -1,5 +1,7 @@
 #include <benchmark/benchmark.h>
 #include <logger.hh>
+#include <consumer.hh>
+#include <fstream>
 
 namespace klog {
 namespace {
@@ -21,6 +23,18 @@ void bm_disabled_log(benchmark::State& state) {
 }
 
 BENCHMARK(bm_disabled_log);
+
+void bm_ostream_log(benchmark::State& state) {
+  std::ofstream f("/dev/null");
+  consumer_override_guard g(std::make_unique<ostream_consumer>(f));
+  logger<loglevel::info> log("bm");
+
+  for (auto _ : state)
+    log.info("benchmark this");
+}
+
+BENCHMARK(bm_ostream_log);
+
 
 }
 }
